bebae6bc57d41ba114c78cb812ea699192e2c0bc
onexf <630850673@qq.com>
2019-03-03 15:01:36 +0800

    初始化

---
+++ Example/Pods/Target Support Files/PrintChinese/PrintChinese-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PrintChinese : NSObject
+@end
+@implementation PodsDummy_PrintChinese
+@end
---
+++ PrintChinese/Classes/FAQPrintJsonLog.h
@@ -0,0 +1,9 @@
+//
+//  FAQPrintJsonLog.h
+//  PruchaseCarAnimation
+//
+//  Created by 王鑫锋 on 2019/3/3.
+//  Copyright © 2019 com.demo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
